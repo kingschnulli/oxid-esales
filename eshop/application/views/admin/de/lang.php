90a402f8be064aa3bdacb84c5f4aeed6ba92ac36
nightlybuild <nightlybuild@f9fff402-8d1c-4889-a43d-cf23fd1f934a>
2013-01-29 10:19:00 +0000

    OXID eShop Community Edition (features branch) ver. 4.7.3 rev. 54408

--- eshop/application/views/admin/de/lang.php
+++ eshop/application/views/admin/de/lang.php
@@ -19,7 +19,7 @@
  * @package   lang
  * @copyright (C) OXID eSales AG 2003-2013
  * @version OXID eShop CE
- * @version   SVN: $Id: lang.php 53760 2013-01-14 08:17:12Z aurimas.gladutis $
+ * @version   SVN: $Id: lang.php 54386 2013-01-25 10:23:10Z juergen.busch $
  */
 
 $sLangName  = 'Deutsch';
@@ -406,8 +406,8 @@ $aLang = array(
 'ARTICLE_LIST_ALLPRODUCTS'                                 => 'alle Artikel',
 'ARTICLE_LIST_MENUSUBITEM'                                 => 'Artikel',
 'ARTICLE_MAIN_ERRORSAVINGARTICLE'                          => 'Warnung: Artikelnummer bereits vorhanden!',
-'ARTICLE_MAIN_PARENTNOTBUYABLE'                            => '[TR]Attention: Parent Product can not be purchased.',
-'ARTICLE_MAIN_INVALIDTAGSFOUND'                            => '[TR]Attention: Invalid tags removed',
+'ARTICLE_MAIN_PARENTNOTBUYABLE'                            => 'Achtung: Vaterartikel kann nicht gekauft werden.',
+'ARTICLE_MAIN_INVALIDTAGSFOUND'                            => 'Achtung: Unzulässige Tags wurden entfernt.',
 'ARTICLE_MAIN_ALDPRICE'                                    => 'Alt. Preise',
 'ARTICLE_MAIN_NONE'                                        => '-- keine --',
 'ARTICLE_MAIN_GOTO'                                        => 'Gehe zu',
